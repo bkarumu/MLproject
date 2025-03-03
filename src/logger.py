7d80eb5097af199186972fd959bd5d076a277f82
bkarumu <bhavikareddy.karumuri@gmail.com>
2025-03-03 10:54:52 -0600

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+# to log any execution that happens while coding
+# track errors
+
+
+import logging
+import os
+from datetime import datetime
+
+# how my file should look like?
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs",log_file)
+
+os.makedirs(logs_path,exist_ok=True)
+
+log_file_path = os.path.join(logs_path,log_file)
+
+
+logging.basicConfig(
+    filename=log_file_path,
+    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
